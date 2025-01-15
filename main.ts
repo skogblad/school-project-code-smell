0828b198879a1344542ff0c2b79a87fa7723152b
Alicia <a.skogblad@gmail.com>
2025-01-15 10:48:28 +0100

    (perf): updated js files to .ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+import "./style.scss";
+import { createHtml } from "./src/ts/createHtml.mjs";
+import toggleLightMode from "./src/ts/toggleDarkmode.mjs";
+
+
+function init() {
+  toggleLightMode();
+  createHtml();
+}
+
+init();
