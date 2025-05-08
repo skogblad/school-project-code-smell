dddf1b41cb625e7a17f521e29c1b1d0f0a89ac9a
Alicia <a.skogblad@gmail.com>
2025-05-08 11:51:58 +0200

    (refactor): added missing / in base link

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/skogblad/school-project-code-smell", 
+  base: "/skogblad/school-project-code-smell/", 
   build: {
     outDir: "dist",
   },
